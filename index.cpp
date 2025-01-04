e53eac1e386cdbe220a859e3801c0d92344ce380
Gabriel <gfellipe13cc@gmail.com>
2025-01-04 19:41:37 -0300

    04/01 last changes

--- index.cpp
+++ index.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+int score = 0; // Quando chegar em 5, o usuário deve perder
+
+void clearConsole()
+{
+    #ifdef _WIN32
+        system("cls"); // Comando para Windows
+    #else
+        system("clear"); // Comando para Linux/macOS
+    #endif
+}
+
 struct Letter
 { // Estrutura para armazenar cada caractere da palavra escolhida
     char letter;
@@ -17,9 +28,9 @@ Letter *storeWord(const string &word)
     for (char c : word)
     {
         Letter *newLetter = new Letter; // Alocar nova letra
-        newLetter->letter = c;          // Armazenar letra
+        newLetter->letter = tolower(c); // Armazenar letra em minuscula
         newLetter->isGuessed = false;   // Deixa não inicializado como padrão
-        newLetter->next = nullptr;      // Mostra o caminho para próxima letr
+        newLetter->next = nullptr;      // Mostra o caminho para próxima letra
 
         if (head == nullptr)
         {
@@ -53,58 +64,110 @@ void showWord(Letter *head)
     }
 }
 
+struct MissedLetter
+{ // Estrutura para armazenar cada letra errada
+    char letter;
+    MissedLetter *next;
+};
+
+MissedLetter *storeMissedWord(const string &word)
+{ // Lista com as letras erradas
+    MissedLetter *head = nullptr;
+    MissedLetter *tail = nullptr;
+    for (char c : word)
+    {
+        MissedLetter *newMissedLetter = new MissedLetter; // Alocar nova letra
+        newMissedLetter->letter = tolower(c); // Armazenar letra em minuscula
+        newMissedLetter->next = nullptr; // Mostra o caminho para próxima letra
+
+        if (head == nullptr)
+        {
+            head = newMissedLetter; // Detecta se é a primeira letra
+        }
+        else
+        {
+            tail->next = newMissedLetter; // Se a lista não está vazia, a nova letra é adicionada ao final
+        }
+
+        tail = newMissedLetter; // Muda a tail para a nova letra
+    }
+
+    return head;
+}
+
+bool guessLetter(Letter *head, char guess)
+{
+    Letter *current = head;
+    bool found = false;
+    while (current != nullptr) // Percorre a lista de letras e confere se tem alguma letra igual ao guess
+    {
+        if (current->letter == guess && !current->isGuessed)
+        {
+            current->isGuessed = true; // Marca a letra como adivinhada
+            found = true;
+        }
+        current = current->next;
+    }
+    return found;
+}
+
+void startGame(Letter *chosenWord)
+{
+    char guess;
+    // comparar score, se maior que 5, sai do loop e mensagem de perdeu
+    while (score < 5)
+    {
+        // mostrar o boneco
+        cout << "Digite uma letra!" << endl;
+        cin >> guess;
+        guess = tolower(guess); // Deixa minúscula
+
+        //if (guess ==)
+            // quando a letra foi escolhida, verificar se ela está na palavra
+            // se tiver, mudar o parametro isGuessed daquela letra pra verdadeiro
+
+            showWord(chosenWord);
+        // se resta mais chances e todos os isGuessed estão true:
+        // mensagem que ganhou
+        // se não, chama a função startGame(Letter *chosenWord) de novo
+    }
+
+    cout << "Sem mais tentativas! A palavra era: ";
+    Letter *current = chosenWord;
+    while (current != nullptr)
+    {
+        cout << current->letter;
+        current = current->next;
+    }
+    cout << endl;
+}
+
 /*void showAvatar()
 {
     cout << "_______" << endl;
     cout << "|     |" << endl;
-    cout << "|   (o-o)" << endl;  // errou aqui
-    cout << "|    /|\\" << endl; // depois aqui mostrando
+    cout << "|   (o-o)" << endl;
+    cout << "|    /|\\" << endl;
     cout << "|     |   " << endl;
     cout << "|    / \\ " << endl;
 }
 */
 
-/*void menu()
-{
-    int opcao;
-    do{
-        cout<<"0 - Digitar palavra a ser adivinhada"<<endl;
-        cout <<"1 - Advinhar a palavra" << endl;
-        cout <<"2 - Estado atual do Avatar" << endl;
-        cout <<"3 - Letras usada" << endl;
-        cout <<"4 - Encerrar programa"<<endl;
-        cin>>opcao; //Palavra escolhida
-
-        switch(opcao){
-            case 0:
-            digitarpalavra();
-            break;
-            case 1:
-            advinharpalavra();
-            break;
-            case 2:
-            showAvatar();
-            break;
-            case 3:
-            showLetrasUsadas();
-            break;
-            default:
-            cout<<"opção invalida!"<<endl;
-            break;
-        }
-    while(opcao != 4);
-   }
-
-}*/
-
 int main()
 {
-    setlocale(LC_ALL, "pt_BR.UTF-8");
+    setlocale(LC_ALL, "Portuguese");
+    cout << "========== JOGO DA FORCA ==========" << endl;
 
     string word;
-    cout << "JOGADOR 1 - Escolha uma palavra a ser adivinhada (utilize apenas letras e sem acento): " << endl;
+    cout << "JOGADOR 1 - Escolha uma palavra a ser adivinhada (utilize apenas letras e sem acento): ";
     cin >> word;
 
     Letter *chosenWord = storeWord(word);
-    showWord(chosenWord);
+    cout << "JOGADOR 1 - Palavra escolhida com sucesso! Pressione ENTER para iniciar o jogo de JOGADOR 2" << endl;
+    cin.ignore();
+    cin.get();
+
+    clearConsole();
+
+    startGame(chosenWord);
 }
---
+++ todo.md
@@ -0,0 +1,15 @@
+[-] Inicialize o jogo esperando o usuário 1 escrever uma palavra, crie uma
+“máscara” para esconder tal palavra e lembre-se de definir um número de
+tentativas.
+
+[-] Receber entrada do usuário 2, sem letras repetidas.
+
+[ ] Função para atualizar e exibir o estado atual do jogo para o jogador. Mostre
+as letras já descobertas da palavra (ou espaços em branco) e as letras
+erradas que foram tentadas.
+
+[-] Forneça uma interface de usuário simples para interagir com o jogo.
+
+-------
+
+[ ]
