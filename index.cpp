c2bdcc281abd53be5d79c01e05466c554d57e81a
Gabriel <gfellipe13cc@gmail.com>
2025-01-07 19:45:31 -0300

    finishing

--- index.cpp
+++ index.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <locale>
+#include <unordered_set>
 
 using namespace std;
 
@@ -12,6 +13,7 @@ void clearConsole()
 #else
     system("clear"); // Comando para Linux/macOS
 #endif
+cout << "\n========== JOGO DA FORCA ==========" << endl;
 }
 
 struct Letter
@@ -160,7 +162,7 @@ MissedLetter *storeMissedWord(const string &word)
 
 void showMissedLetters(MissedLetter *head)
 {
-    cout << "Letras erradas: ";
+    cout << "\nLetras erradas: ";
     MissedLetter *current = head;
     while (current != nullptr)
     {
@@ -204,10 +206,35 @@ bool hasGuessedAll(Letter *head)
     return true;
 }
 
+// Função que altera o estado isGuessed de cada letra
+void updateGuessedLetters(Letter *head, char guess)
+{
+    Letter *current = head;
+    while (current != nullptr)
+    {
+        if (current->letter == guess)
+        {
+            current->isGuessed = true;
+        }
+        current = current->next;
+    }
+}
+
+bool containsAccent(const string &word)
+{
+    for (char c : word) {
+        if(!isLetter(c)) {
+            return true;
+        };
+    }
+    return false;
+}
+
 void startGame(Letter *chosenWord)
 {   
     MissedLetter *missedLetters = nullptr;
     char guess;
+    unordered_set<char> guessedLetters; // Conjunto para armazenar letras já tentadas
     // comparar score, se maior que 5, sai do loop e mensagem de perdeu
     while (score < 5)
     {
@@ -215,19 +242,41 @@ void startGame(Letter *chosenWord)
         showWord(chosenWord);
         showMissedLetters(missedLetters);
 
-        if(hasGuessedAll(cho))
-
-        cout << "\nDigite uma letra!" << endl;
+        
+        cout << "\nDigite uma letra: ";
         cin >> guess;
         if(isLetter(guess)) {
             guess = tolower(guess); // Deixa minúscula
+            
+            if (guessedLetters.find(guess) != guessedLetters.end())
+            {
+                clearConsole();
+                cout << "Letra repetida, tente novamente! ";
+                continue; // Se já foi tentada, pede nova letra
+            }
+            guessedLetters.insert(guess); // Adiciona a letra ao conjunto de letras tentadas
+
+            // Verificar se a letra está na palavra
+            if (!guessLetter(chosenWord, guess))
+            {
+                // Se a letra não está na palavra, incrementa o score e armazena a letra errada
+                MissedLetter *newMissedLetter = new MissedLetter;
+                newMissedLetter->letter = guess;
+                newMissedLetter->next = missedLetters;
+                missedLetters = newMissedLetter;
+                score++;
+            }
+            if(hasGuessedAll(chosenWord)){
+                clearConsole();
+                cout<<"Palavra adivinhada! ";
+                break;
+            }
             clearConsole();
-        } else {
+        }
+        else {
             clearConsole();
             cout<<"O caractere digitado deve ser uma letra sem acento! ";// Mensagem de erro 
         }
-
-        
 
         // quando a letra foi escolhida, verificar se ela está na palavra
         // se tiver, mudar o parametro isGuessed daquela letra pra verdadeiro
@@ -235,27 +284,67 @@ void startGame(Letter *chosenWord)
         // se resta mais chances e todos os isGuessed estão true:
         // mensagem que ganhou
         // se não, chama a função startGame(Letter *chosenWord) de novo
+    
+    if(score==5){
+        cout << "Sem mais tentativas! ";
     }
+   }
+}
 
-    cout << "Sem mais tentativas! A palavra era: ";
-    Letter *current = chosenWord;
-    while (current != nullptr)
-    {
-        cout << current->letter;
-        current = current->next;
-    }
-    cout << endl;
+void menu() {
+    cout << "1. Iniciar o jogo" << endl;
+    cout << "2. Regras" << endl;
+    cout << "3. Sair" << endl;
+    cout << "Escolha: ";
+    int choice;
+    cin >> choice;
+    switch (choice) {
+    case 1:
+        clearConsole();
+        break;
+    case 2:
+        clearConsole();
+        cout << "REGRAS DO JOGO:" << endl;
+        cout << "- O JOGADOR 1 escolhe uma palavra sem caracteres especiais ou acentos.\n- O JOGADOR 2 tenta adivinhar a palavra, letra por letra.\n- Para cada erro, aparece um novo desenho complementando o boneco da forca.\n- O jogo termina quando o JOGADOR 2 adivinha a palavra ou esgota as 5 tentativas." << endl;
+        cout << "Pressione ENTER para voltar ao menu..." << endl;
+        cin.ignore();
+        cin.get();
+        clearConsole();
+        menu();
+        break;
+    case 3:
+        clearConsole();
+        cout << "Obrigado por jogar!" << endl;
+        exit(0);
+        break;
+    case 4: 
+        clearConsole();
+        cout<<"Obrigado pelos ensinamento :D" << endl;
+        menu();
+        break;
+    default:
+        clearConsole();
+        cout << "Escolha de 1 a 3." << endl;
+        menu();
+        break;
+}
 }
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    cout << "========== JOGO DA FORCA ==========" << endl;
-
+    cout << "\n========== JOGO DA FORCA ==========" << endl;
+    menu();
     string word;
-    cout << "JOGADOR 1 - Escolha uma palavra a ser adivinhada (utilize apenas letras e sem acento): ";
-    cin >> word;
 
+    do{
+        cout << "JOGADOR 1 - Escolha uma palavra a ser adivinhada (utilize apenas letras e sem acento): ";
+        cin >> word;
+        if(containsAccent(word)){
+            cout << "Tente novamente com uma palavra sem acentos ou caracteres especiais." << endl;
+        }
+    }while(containsAccent(word));
+    
     Letter *chosenWord = storeWord(word);
     cout << "JOGADOR 1 - Palavra escolhida com sucesso! Pressione ENTER para iniciar o jogo de JOGADOR 2" << endl;
     cin.ignore();
@@ -264,4 +353,5 @@ int main()
     clearConsole();
 
     startGame(chosenWord);
+    cout << "A palavra escolhida foi " << word << "." << endl;
 }
--- todo.md
+++ todo.md
@@ -2,21 +2,12 @@
 “máscara” para esconder tal palavra e lembre-se de definir um número de
 tentativas.
 
-[ ] Receber entrada do usuário 2, sem letras repetidas.
+[v] Receber entrada do usuário 2, sem letras repetidas.
 
-[-] Função para atualizar e exibir o estado atual do jogo para o jogador. Mostre
+[v] Função para atualizar e exibir o estado atual do jogo para o jogador. Mostre
 as letras já descobertas da palavra (ou espaços em branco) e as letras
 erradas que foram tentadas.
 
 [v] Forneça uma interface de usuário simples para interagir com o jogo.
 
--------
-
-TODO:
-
-* Não permitir palavras acentuadas no input do jogador 1;
-* Armazenar e mostrar letras erradas;
-* Diferenciar letras repetidas;
-* Função que altera o estado isGuessed de cada letra;
-* Função que detecta se todas as letras já foram adivinhadas;
-
+------
